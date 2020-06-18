3ccc1a116f06833bdcac8912571a3e7b7b56e81f
David Flores <drfloresj@gmail.com>
2020-06-18 01:44:14 -0700

    pushing build to remote repo

---
+++ client/build/precache-manifest.0f6d80a2c18f33d72624a88383c7d911.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92dea6ef605f062d8770",
+    "url": "/static/css/main.40e232a1.chunk.css"
+  },
+  {
+    "revision": "92dea6ef605f062d8770",
+    "url": "/static/js/main.95fc14b1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a410fa716a4873f9219e",
+    "url": "/static/js/2.a002522c.chunk.js"
+  },
+  {
+    "revision": "6b86627d81839f9cefc45d742e40a705",
+    "url": "/index.html"
+  }
+];
